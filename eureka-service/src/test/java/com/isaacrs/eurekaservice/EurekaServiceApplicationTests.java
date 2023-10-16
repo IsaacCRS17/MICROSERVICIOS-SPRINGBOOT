eb04a96ed59f2048dcb217322f9ae8384ef9b0ab
IsaacCRS17 <crs170919@gmail.com>
2023-10-16 15:09:04 -0500

    add eureka

---
+++ eureka-service/src/test/java/com/isaacrs/eurekaservice/EurekaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isaacrs.eurekaservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
