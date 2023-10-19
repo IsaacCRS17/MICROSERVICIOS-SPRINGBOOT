78f927bae92c8f15996cdfdbdbc70e326d911aea
IsaacCRS17 <crs170919@gmail.com>
2023-10-18 20:40:07 -0500

    add circuit breaker with resilience4j

--- user-service/src/main/java/com/isaacrs/userservice/controller/UserController.java
+++ user-service/src/main/java/com/isaacrs/userservice/controller/UserController.java
@@ -4,7 +4,9 @@ import com.isaacrs.userservice.entity.User;
 import com.isaacrs.userservice.model.Bike;
 import com.isaacrs.userservice.model.Car;
 import com.isaacrs.userservice.service.UserService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -45,6 +47,7 @@ public class UserController {
     }
 
     // Obtiene todos los coches de un usuario por su ID
+    @CircuitBreaker(name = "carsCB", fallbackMethod = "fallBackGetCars") //Cortacircuito de resilience4j
     @GetMapping("/cars/{userId}")
     public ResponseEntity<List<Car>> getCarsByUserId(@PathVariable("userId") int userId) {
         User user = userService.getUserById(userId);
@@ -54,8 +57,18 @@ public class UserController {
         List<Car> cars = userService.getCars(userId);
         return ResponseEntity.ok(cars);
     }
-
+    // Crea un nuevo car al usuario
+    @CircuitBreaker(name = "carsCB", fallbackMethod = "fallBackSaveCar") //Cortacircuito de resilience4j
+    @PostMapping("/savecar/{userId}")
+    public ResponseEntity<Car> saveCar(@PathVariable("userId") int userId, @RequestBody Car car){
+        if(userService.getUserById(userId) == null) {
+            return ResponseEntity.notFound().build();
+        }
+        Car carNew = userService.saveCar(userId, car);
+        return ResponseEntity.ok(carNew);
+    }
     // Obtiene todas las bicicletas de un usuario por su ID
+    @CircuitBreaker(name = "bikesCB", fallbackMethod = "fallBackGetBikes") //Cortacircuito de resilience4j
     @GetMapping("/bikes/{userId}")
     public ResponseEntity<List<Bike>> getBikesByUserId(@PathVariable("userId") int userId) {
         User user = userService.getUserById(userId);
@@ -65,16 +78,9 @@ public class UserController {
         List<Bike> bikes = userService.getBikes(userId);
         return ResponseEntity.ok(bikes);
     }
-    // Crea un nuevo car al usuario
-    @PostMapping("/savecar/{userId}")
-    public ResponseEntity<Car> saveCar(@PathVariable("userId") int userId, @RequestBody Car car){
-        if(userService.getUserById(userId) == null) {
-            return ResponseEntity.notFound().build();
-        }
-        Car carNew = userService.saveCar(userId, car);
-        return ResponseEntity.ok(carNew);
-    }
+
     // Crea una nueva bike al usuario
+    @CircuitBreaker(name = "bikesCB", fallbackMethod = "fallBackSaveBike") //Cortacircuito de resilience4j
     @PostMapping("/savebike/{userId}")
     public ResponseEntity<Bike> saveBike(@PathVariable("userId") int userId, @RequestBody Bike bike){
         if(userService.getUserById(userId) == null) {
@@ -84,9 +90,34 @@ public class UserController {
         return ResponseEntity.ok(bikeNew);
     }
     // Obtiene todas las bicicletas y carros de un usuario por su ID
+    @CircuitBreaker(name = "allCB", fallbackMethod = "fallBackGetAll") //Cortacircuito de resilience4j
     @GetMapping("/getAll/{userId}")
     public ResponseEntity<Map<String, Object>> getAllVehicles(@PathVariable("userId") int userId){
         Map<String, Object> result = userService.getUserAndVehicles(userId);
         return ResponseEntity.ok(result);
     }
-}
+    //Método de cortacircuitos de getCars
+    public ResponseEntity<List<Car>> fallBackGetCars(@PathVariable("userId") int userId, RuntimeException e) {
+        return new ResponseEntity("El usuario " + userId + " tiene los carros en el taller", HttpStatus.OK);
+    }
+
+    //Método de cortacircuitos de saveCar
+    public ResponseEntity<Car> fallBackSaveCar(@PathVariable("userId") int userId, @RequestBody Car car, RuntimeException e) {
+        return new ResponseEntity("El usuario " + userId + " no tiene dinero para carros", HttpStatus.OK);
+    }
+    //Método de cortacircuitos de getBikes
+    public ResponseEntity<List<Bike>> fallBackGetBikes(@PathVariable("userId") int userId, RuntimeException e) {
+        return new ResponseEntity("El usuario " + userId + " tiene las motos en el taller", HttpStatus.OK);
+    }
+
+    //Método de cortacircuitos de saveBike
+    public ResponseEntity<Bike> fallBackSaveBike(@PathVariable("userId") int userId, @RequestBody Bike bike, RuntimeException e) {
+        return new ResponseEntity("El usuario " + userId + " no tiene dinero para motos", HttpStatus.OK);
+    }
+    //Método de cortacircuitos de allVehicles
+
+    public ResponseEntity<Map<String, Object>> fallBackGetAll(@PathVariable("userId") int userId, RuntimeException e){
+        return new ResponseEntity("El usuario " + userId + " tiene los vehiculos en el taller", HttpStatus.OK);
+    }
+
+    }
